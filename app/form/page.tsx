ec6639a02e079c6a1e99254ee6531a2411bc1f61
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2025-01-17 15:34:23 +0800

    测试prisma

---
+++ app/form/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FormDemo() {
+    return (
+        <div>FormDemo</div>
+    );
+}
+
+export default FormDemo;
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,78 @@
+import {Calendar, Home, Inbox, Search, Settings, TableIcon} from "lucide-react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import {FormatDetectionMeta} from "next/dist/lib/metadata/generate/basic";
+
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Inbox",
+        url: "#",
+        icon: Inbox,
+    },
+    {
+        title: "Calendar",
+        url: "#",
+        icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "#",
+        icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+    {
+        title: "tables",
+        url: "/table",
+        icon: TableIcon,
+    },
+    {
+        title: "form",
+        url: "/form",
+        icon: Search,
+    }
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
